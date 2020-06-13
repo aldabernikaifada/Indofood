8be85fff08292eeac5a0a92ae1d72ac2b75a92ce
RisaNailiFitriana <risanaili@students.unnes.ac.id>
2020-06-13 10:17:36 +0700

    rubah dikit

--- application/views/v_dashboard.php
+++ application/views/v_dashboard.php
@@ -1,4 +1,3 @@
-
 <div class="col-md-12">
           <!-- Widget: user widget style 1 -->
           <div class="box box-widget widget-user">
@@ -7,11 +6,8 @@
              <!--  <h3 class="widget-user-username"><?=from_session('nama')?></h3>
               <h5 class="widget-user-desc"><?=from_session('nama_level')?></h5>
             </div>
-            <div class="widget-user-image"> -->
-
-            
-            </div>
-            <div class="box-footer">
+            <div class="widget-user-image"> -->          
+    <div class="box-footer">
               <div class="row">
                 <div class="col-sm-12 border-right">
                   <div class="description-block">
@@ -19,7 +15,7 @@
                   $avatar = parse_avatar(from_session('gambar'),from_session('nama'),40,'img-circle');
                 ?>
                 <?php echo $avatar; ?>
-                    <h5 class="description-header">SELAMAT DATANG DI <?php echo $this->config->item('project')?></h5>
+                    <h5 class="description-header">SELAMAT DATANG DI GUBUG MART</h5>
                     <span class="description-text"><?php echo button('load_silent("cms/user/show_editForm_user/'.from_session('id').'","#content")','Update Profil','btn bg-red','data-toggle="tooltip" title="Update Profil"');?> 
 						</span>
                   </div>
@@ -32,4 +28,99 @@
             </div>
           </div>
           <!-- /.widget-user -->
-        </div>
+          </div>
+<!-- Main content --> 
+		<section class="content">
+		<!-- Small boxes (Stat box) --> 
+		<div class="row">
+		<div class="col-lg-3 col-xs-6">
+		<!-- small box -->
+		<div class="small-box bg-green">
+		<div class="inner">
+		<h3>150</h3>
+		<p>Customer</p>
+		</div>
+		<div class="icon">
+		<i class="fa fa-cubes"></i>
+		</div>
+		<a href="?page" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+		</div>
+		</div>
+
+    <!-- Small boxes (Stat box) --> 
+		<div class="row">
+		<div class="col-lg-3 col-xs-6">
+		<!-- small box --> 
+		<div class="small-box bg-purple">
+		<div class="inner">
+		<h3>20</h3>
+		<p>Supplier</p>
+		</div>
+		<div class="icon">
+		<i class="fa fa-cubes"></i>
+		</div>
+		<a href="application/kelola/supplier" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+		</div>
+		</div>
+
+		<!-- ./col --> 
+		<div class="col-lg-3 col-xs-6">
+		<!-- small box --> 
+		<div class="small-box bg-orange">
+		<div class="inner">
+		<h3>200</h3>
+		<p>Produk</p>
+		</div>
+		<div class="icon">
+		<i class="fa fa-cubes"></i>
+		</div>
+		<a href="?page" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+		</div>
+    </div>
+
+		<!-- ./col -->
+		<div class="col-lg-3 col-xs-6">
+		<!-- small box --> 
+		<div class="small-box bg-green">
+		<div class="inner">
+		<h3>65</h3> 
+		<p>Purchase</p>
+		</div>
+		<div class="icon">
+    <i class="fa fa-cubes"></i>
+		</div>
+		<a href="?page" class="small-box-footer">More info <i class ="fa fa-arrow-circle-right"></i></a>
+		</div>
+		</div>
+
+    	<!-- ./col -->
+		<div class="col-lg-3 col-xs-6">
+		<!-- small box --> 
+		<div class="small-box bg-purple">
+		<div class="inner">
+		<h3>200</h3> 
+		<p>Stok</p>
+		</div>
+		<div class="icon">
+    <i class="fa fa-cubes"></i>
+		</div>
+		<a href="?page" class="small-box-footer">More info <i class ="fa fa-arrow-circle-right"></i></a>
+		</div>
+		</div>
+
+    	<!-- ./col -->
+		<div class="col-lg-3 col-xs-6">
+		<!-- small box --> 
+		<div class="small-box bg-red">
+		<div class="inner">
+		<h3>65</h3> 
+		<p>Pengembalian</p>
+		</div>
+		<div class="icon">
+    <i class="fa fa-cubes"></i>
+		</div>
+		<a href="?page" class="small-box-footer">More info <i class ="fa fa-arrow-circle-right"></i></a>
+		</div>
+		</div>
+    </div>
+    
