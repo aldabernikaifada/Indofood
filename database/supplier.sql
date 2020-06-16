c6c0bb0ccc08ca23c7a9b0c820669cd1f4f9214c
aldabernikaifada <64902871+aldabernikaifada@users.noreply.github.com>
2020-06-16 08:28:34 +0700

    edit db supplier

--- database/supplier.sql
+++ database/supplier.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 12, 2020 at 05:51 PM
+-- Generation Time: Jun 16, 2020 at 03:27 AM
 -- Server version: 10.1.32-MariaDB
 -- PHP Version: 5.6.36
 
@@ -32,6 +32,7 @@ CREATE TABLE `supplier` (
   `id` int(255) NOT NULL,
   `kode_supplier` varchar(255) NOT NULL,
   `nama_supplier` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
   `telepon` int(255) NOT NULL,
   `keterangan` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -40,9 +41,9 @@ CREATE TABLE `supplier` (
 -- Dumping data for table `supplier`
 --
 
-INSERT INTO `supplier` (`id`, `kode_supplier`, `nama_supplier`, `telepon`, `keterangan`) VALUES
-(1, 'BM0987', 'Danang Setia', 2147483647, 'Supplier Bahan Makanan'),
-(2, 'MR87637', 'Tasya Mawardah', 982738273, 'Supplier Makanan Ringan');
+INSERT INTO `supplier` (`id`, `kode_supplier`, `nama_supplier`, `alamat`, `telepon`, `keterangan`) VALUES
+(1, 'BM0987', 'Danang Setia', 'Jakarta', 2147483647, 'Supplier Bahan Makanan'),
+(2, 'MR87637', 'Tasya Mawardah', 'Semarang', 982738272, 'Supplier Makanan Ringan');
 
 --
 -- Indexes for dumped tables
